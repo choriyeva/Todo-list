9797a2a140beebdd6ba20ca09bfcd2d7ba347a94
choriyeva <rozigulchoriyeva15@gmail.com>
2023-11-28 21:25:52 +0500

    todo

--- script.js
+++ script.js
@@ -43,20 +43,20 @@ function showData(){
 
 showData()
 
-// document.onkeydown = (e) => {
-//     if (e.key == 123) {
-//         e.preventDefault();
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.key == 'I') {
-//         e.preventDefault();
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.key == 'C') {
-//         e.preventDefault();
-//     }
-//     if (e.ctrlKey && e.shiftKey && e.key == 'J') {
-//         e.preventDefault();
-//     }
-//     if (e.ctrlKey && e.key == 'U') {
-//         e.preventDefault();
-//     }
-// };
+document.onkeydown = (e) => {
+    if (e.key == 123) {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.key == 'U') {
+        e.preventDefault();
+    }
+};
